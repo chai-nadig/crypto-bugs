b693494bc0af2f531764cbd3ae2ab54cd81c434f
chai-nadig <chaitanya.nadig@gmail.com>
2021-10-28 22:31:56 -0700

    initial

---
+++ LadyBug/combine.py
@@ -0,0 +1,40 @@
+import os
+
+from PIL import Image, ImageOps
+
+directory = "./output"
+images = [file for file in os.listdir(directory) if file.endswith(".png")]
+total = len(images)
+
+print("combining {} files".format(total))
+
+
+def getNearestSquare(n):
+    i = 1
+    while i * i < n:
+        i += 1
+
+    return i
+
+
+ns = getNearestSquare(total)
+
+final = Image.new('RGBA', (ns * 24, ns * 24))
+
+print(images)
+
+imgNo = 0
+
+for i in range(ns):
+    for j in range(ns):
+        if len(images) == 0 or imgNo >= total:
+            continue
+
+        file = '{}.png'.format(imgNo)
+        im1 = Image.open(directory + '/' + file).convert('RGBA')
+
+        final.paste(im1, (i * 24, j * 24))
+
+        imgNo += 1
+
+final.save('combined.png')
---
+++ LadyBug/generate.py
@@ -0,0 +1,159 @@
+from PIL import Image, ImageOps
+from IPython.display import display
+import random
+import json
+
+backgrounds = {
+    "SpiderWeb": 1,
+    "Stick": 1,
+    "Leaf": 1,
+    "Hearts": 1,
+    "Book": 1,
+    "Matrix": 1,
+    "Rainbow1": 1,
+    "Rainbow2": 1,
+    "Rainbow3": 1,
+}
+
+bugs = {
+    "Small": 1,
+}
+
+smallSpots = {
+    "SmallBlackSpotsA": 1,
+    "SmallBlackSpotsB": 1,
+    "SmallBlackSpotsC": 1,
+    "SmallDarkRedSpotsA": 1,
+    "SmallDarkRedSpotsB": 1,
+    "SmallDarkRedSpotsC": 1,
+    "SmallYellowSpotsA": 1,
+    "SmallYellowSpotsB": 1,
+    "SmallYellowSpotsC": 1,
+}
+smallColors = {
+    "SmallRed": 1,
+    "SmallBlue": 1,
+    "SmallBlack": 1,
+    "SmallGreen": 1,
+    "SmallINFlag": 1,
+    "SmallYellow": 1,
+}
+
+accessories = {
+    "Sombrero": 1,
+    "TopHat": 1,
+    "Turban": 1,
+}
+
+eyes = {
+    "BlueEyes": 1,
+    "RedEyes": 1,
+    "WhiteEyes": 1,
+}
+
+TOTAL_BUGS = (
+        len(backgrounds)
+        * len(bugs)
+        * len(smallSpots)
+        * len(smallColors)
+        * len(accessories)
+        * len(eyes)
+)
+
+traits = []
+
+
+def createCombo():
+    trait = {
+        "Background": random.choices(list(backgrounds.keys()), list(backgrounds.values()))[0],
+        "Bug": random.choices(list(bugs.keys()), list(bugs.values()))[0],
+        "Spots": random.choices(list(smallSpots.keys()), list(smallSpots.values()))[0],
+        "Color": random.choices(list(smallColors.keys()), list(smallColors.values()))[0],
+        "Accessory": random.choices(list(accessories.keys()), list(accessories.values()))[0],
+        "Eyes": random.choices(list(eyes.keys()), list(eyes.values()))[0],
+    }
+
+    return trait
+
+
+for i in range(TOTAL_BUGS):
+    trait = createCombo()
+    while trait in traits:
+        trait = createCombo()
+
+    traits.append(trait)
+
+
+def allUnique(x):
+    seen = list()
+    return not any(i in seen or seen.append(i) for i in x)
+
+
+# print(allUnique(traits))
+
+
+# Sort for evaluation
+traits = sorted(traits, key=lambda t: (t['Background'], t['Color'], t['Spots'], t['Accessory'], t['Eyes']))
+
+# ADD TOKEN IDS TO JSON
+i = 0
+for item in traits:
+    item["tokenId"] = i
+    i = i + 1
+
+# print(traits)
+
+
+# GET TRAIT COUNTS
+from collections import defaultdict
+
+backgroundcounts = defaultdict(int)
+bugcounts = defaultdict(int)
+spotscounts = defaultdict(int)
+colorcounts = defaultdict(int)
+accessorycount = defaultdict(int)
+eyescount = defaultdict(int)
+
+for bug in traits:
+    backgroundcounts[bug["Background"]] += 1
+    bugcounts[bug["Bug"]] += 1
+    spotscounts[bug["Spots"]] += 1
+    colorcounts[bug["Color"]] += 1
+    accessorycount[bug["Accessory"]] += 1
+    eyescount[bug["Eyes"]] += 1
+
+print("background:", backgroundcounts)
+print("bugs:", bugcounts)
+print("spots:", spotscounts)
+print("colors:", colorcounts)
+print("accessory:", accessorycount)
+print("eyes:", eyescount)
+
+# WRITE METADATA TO JSON FILE
+
+with open('traits2.json', 'w') as outfile:
+    json.dump(traits, outfile, indent=4)
+
+# IMAGE GENERATION
+for item in traits:
+    im1 = Image.open(f'./Backgrounds/{item["Background"]}.png').convert('RGBA')
+    im2 = Image.open(f'./Bugs/{item["Bug"]}.png').convert('RGBA')
+    im3 = Image.open(f'./Colors/{item["Color"]}.png').convert('RGBA')
+    im4 = Image.open(f'./Spots/{item["Spots"]}.png').convert('RGBA')
+    im5 = Image.open(f'./Accessories/{item["Accessory"]}.png').convert('RGBA')
+    im6 = Image.open(f'./Eyes/{item["Eyes"]}.png').convert('RGBA')
+
+    com1 = Image.alpha_composite(im1, im2)
+    com2 = Image.alpha_composite(com1, im3)
+    com3 = Image.alpha_composite(com2, im4)
+    com4 = Image.alpha_composite(com3, im5)
+    com5 = Image.alpha_composite(com4, im6)
+
+    # Convert to RGB
+    rgb_im = com5.convert('RGBA')
+    #     display(rgb_im.resize((400,400), Image.NEAREST))
+
+    file_name = str(item["tokenId"]) + ".png"
+    rgb_im.save("./output/" + file_name)
+    # final.save("./output/" + file_name)
+    print(f'{str(item["tokenId"])} done')
