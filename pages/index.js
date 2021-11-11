19f1678bb6e9505d6e29f35a7d2216f05cac5427
chai-nadig <chaitanya.nadig@gmail.com>
2021-11-11 05:03:30 -0800

    fix

--- pages/index.js
+++ pages/index.js
@@ -210,7 +210,7 @@ return (
       <p class="text-2xl text-white my-6">
         We're three software techies, that got really engrossed in the world of NFT recently. Mostly inspired by
         <a className="text-white-shadow hover:text-black" href="https://www.theverge.com/2021/3/11/22325054/beeple-christies-nft-sale-cost-everydays-69-million" target="_blank"> Beeple's sale for $69 Million</a>
-        &nbsp; and <a className="text-white-shadow hover:text-black" href="https://www.larvalabs.com/cryptopunks" target="_blank"> 24 x 24 bit art by Cryptopunks,</a>
+        &nbsp; and <a className="text-white-shadow hover:text-black" href="https://www.larvalabs.com/cryptopunks" target="_blank"> Cryptopunks' 24 x 24 pixel art,</a>
         &nbsp; we came up with <b className="text-red ">crypto-bugs-0x2b67.</b>
       </p>
       <p class="text-2xl text-white my-6">
