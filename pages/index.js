8fb7472a807b7623871976baa87146a8c3d3f1eb
chai-nadig <chaitanya.nadig@gmail.com>
2021-11-11 18:23:37 -0800

    the lady bug

--- pages/index.js
+++ pages/index.js
@@ -195,7 +195,7 @@ return (
       <div className="flex justify-around flex-wrap">
         <div className="flex flex-col  my-6">
           <div className="cards-image-mask"><img src="" width="360px" alt="" className="cards-image" /></div>
-          <h3 className="my-4 text-center text-5xl text-center text-red hover:text-red">uv</h3>
+          <h3 className="my-4 text-center text-5xl text-center text-red hover:text-red">theladybug</h3>
           <p className="text-center text-white text-2xl">
             <span className="text-3xl">Comms</span><br/>
             uv@crypto-bugs.com
