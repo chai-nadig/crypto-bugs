e0a25323b04789cc66f3f2efa77b776736e1a2f6
chai-nadig <chaitanya.nadig@gmail.com>
2021-11-28 17:57:29 -0800

    clean up

--- pages/index.js
+++ pages/index.js
@@ -75,15 +75,15 @@ return (
   </Head>
 
   <div className="md:w-2/3 w-4/5" >
-    <div id="about" className="md:mx-6 lg:mx-6 py-4 md:py-8 lg:py-8">
-      <div className="mt-3 md:mt-6 lg:mt-6">
-        <h1 className="text-6xl text-center text-crypto-red">crypto-bugs</h1>
+    <div id="about" className="py-10">
+      <div className="py-10">
+        <h1 className="text-5xl mg:text-6xl lg:text-6xl text-center text-crypto-red">crypto-bugs</h1>
         <p className="text-2xl text-center text-white my-6">
           A <span className="italic">loveliness</span> of <span className="text-red">11,111</span> unique lady bugs.
         </p>
       </div>
 
-      <div className="mt-6" >
+      <div className="py-10">
         <div className="items-center">
           <nav className="flex flex-wrap flex-row justify-center">
             <a href="https://twitter.com/CryptoBugsx2B67" target="_blank"><img src="images/twitter.png" width="50px"/></a>
@@ -94,9 +94,9 @@ return (
     </div>
     
     {showTraits ? 
-    <div id="traits" className="mx-2">
-      <h4 className="text-crypto-red text-5xl text-center py-10">TRAITS</h4>
-      <div className="flex flex-wrap px-4 pt-2 pb-7">
+    <div id="traits" className="py-10">
+      <h2 className="text-crypto-red text-5xl text-center">TRAITS</h2>
+      <div className="flex flex-wrap px-4 py-10">
         <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5  grid justify-items-center">
           <span class="text-white text-lg">{traits[0][0]}</span>
           <img src={traits[0][1]} className="w-11/12 rounded-lg"/>
@@ -126,41 +126,40 @@ return (
     : <div></div> }
     
     
-    <div id="lifecycle" className="mx-6 py-4">
-      <h2 className="text-crypto-red text-6xl text-center py-10">LIFECYCLE</h2>
-      
-      <div className="flex flex-wrap items-center py-6">
-        <div className="w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 pb-6">
+    <div id="lifecycle" className="py-10">
+      <h2 className="text-crypto-red text-6xl text-center">LIFECYCLE</h2>
+      <div className="flex flex-wrap items-center py-8">
+        <div className="w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 py-10">
           <img src="images/set3/3.png" className="w-11/12 rounded-lg"/>
         </div>
         <div className="flex flex-col md:w-2/3 lg:w-2/3 md:pr-20 lg:pr-20 text-justify">
-          <span className="text-4xl text-white text-center md:text-left lg:text-left "><span className="text-red ">0%</span> Eggs</span>
+          <span className="text-4xl text-white text-center md:text-left lg:text-left py-3"><span className="text-red ">0%</span> Eggs</span>
           <p class="text-xl text-white">
-             The eggs are laid. The loveliness of ladybugs is yet to hatch. We're running a giveaway where the first 111 bugs can be adopted for free.
+             The eggs are laid. A loveliness of ladybugs is yet to hatch. We're running a giveaway where the first 111 ladybugs will be adopted for free.
           </p>
         </div>
       </div>
       
-      <div className="flex flex-wrap items-center py-6">
-        <div className="md:order-last lg:order-last w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 pb-6">
+      <div className="flex flex-wrap items-center py-8">
+        <div className="md:order-last lg:order-last w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 py-10">
           <img src="images/set3/2.png" className="w-11/12 rounded-lg"/>
         </div>
 
         <div className="flex flex-col md:w-2/3 lg:w-2/3 md:pl-10 lg:pl-10 text-justify">
-          <span className="text-4xl text-white text-center md:text-right lg:text-right"><span className="text-red ">11%</span> Larva</span>
-          <p class="text-xl text-white md:text-right lg:text-right">
-            The lovelines has hatched. Once 11% of our loveliness is adopted, we will choose a trait and all bugs with the 
+          <span className="text-4xl text-white text-center md:text-right lg:text-right py-3"><span className="text-red ">11%</span> Larva</span>
+          <p class="text-xl text-white">
+            The lovelines has hatched. Once 11% of our loveliness is adopted, we will choose a trait and all ladybugs with the 
             chosen trait will be printed on T-Shirts.
           </p>
         </div>
       </div>
       
-      <div className="flex flex-wrap items-center py-6">
-        <div className="w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 pb-6">
+      <div className="flex flex-wrap items-center py-8">
+        <div className="w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 py-10">
           <img src="images/set3/5.png" className="w-11/12 rounded-lg"/>
         </div>
         <div className="flex flex-col md:w-2/3 lg:w-2/3 md:pr-20 lg:pr-20 text-justify">
-          <span className="text-4xl text-white text-center md:text-left lg:text-left "><span className="text-red ">33%</span> Pupa</span>
+          <span className="text-4xl text-white text-center md:text-left lg:text-left py-3"><span className="text-red ">33%</span> Pupa</span>
           <p class="text-xl text-white">
              Our ladybugs have started showing colors at this stage. To ensure that they have a healthy environment to grow,  
              we will donate to <a className="text-red hover:text-white" href="https://www.catf.us/" target="_blank">Clean Air Task Force.</a> 
@@ -169,25 +168,25 @@ return (
         </div>
       </div>
       
-      <div className="flex flex-wrap items-center py-6">
+      <div className="flex flex-wrap items-center py-8">
         <div className="md:order-last lg:order-last w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 pb-6">
           <img src="images/set3/1.jpeg"  className="w-11/12 rounded-lg"/>
         </div>
         <div className="flex flex-col md:w-2/3 lg:w-2/3 md:pl-10 lg:pl-10 text-justify">
-          <span className="text-4xl text-white text-center md:text-right lg:text-right"><span className="text-red ">66%</span> Young Adults</span>
-          <p class="text-xl text-white md:text-right lg:text-right">
+          <span className="text-4xl text-white text-center md:text-right lg:text-right py-3"><span className="text-red ">66%</span> Young Adults</span>
+          <p class="text-xl text-white">
              The ladybugs are a loveliness of young adults. They all deserve to be celebrated. 
              We will make banners for every ladybug, so you can show them off on Twitter.
           </p>
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center py-6">
-        <div className="w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 pb-6">
+      <div className="flex flex-wrap items-center py-8">
+        <div className="w-full place-items-center md:w-1/3 lg:w-1/3 grid md:px-20 lg:px-20 px-10 py-10">
           <img src="images/set3/4.png" className="w-11/12 rounded-lg"/>
         </div>
         <div className="flex flex-col md:w-2/3 lg:w-2/3 md:pr-20 lg:pr-20 text-justify">
-        <span className="text-4xl text-white text-center md:text-left lg:text-left "><span className="text-red ">99%</span> Adults</span>
+        <span className="text-4xl text-white text-center md:text-left lg:text-left py-3"><span className="text-red ">99%</span> Adults</span>
           <p class="text-xl text-white">
              All our ladybugs have spread their wings and flown. This time we'll donate to two non-profits that are chosen by the community.
              A total of 29.41 ETH (11.11% of remaining sales) will be donated after 10,999 ladybugs are adopted.
@@ -197,8 +196,8 @@ return (
     </div>
     
     
-    <div id="team" className="mx-12 my-16 border-b-2 py-8">
-      <h2 className="text-crypto-red text-6xl text-center py-10">TEAM</h2>
+    <div id="team" className="py-10">
+      <h2 className="text-crypto-red text-6xl text-center">TEAM</h2>
       <div className="flex justify-around flex-wrap py-10">
         <div className="flex flex-col my-6">
           <div className="cards-image-mask"><img src="" width="360px" alt="" className="cards-image" /></div>
@@ -230,11 +229,9 @@ return (
           <div className="cards-image-mask"><img src="" width="360px" alt="" className="cards-image" /></div>
         </div>
       </div>
-      <p className="text-center py-8">
-         <span class="text-red text-3xl">Join our Discord <u><a className="text-white-shadow hover:text-black" target="_blank" href="https://discord.gg/A6nkdvr2yR">here!</a></u></span>
-      </p>
     </div>
-    <div id="verified-contract" className="mx-12 my-16 border-b-2 py-6">
+    
+    <div id="verified-contract" className="py-10">
       <h2 className="text-crypto-red text-6xl text-center">VERIFIED CONTRACT</h2>
       <div className="flex justify-around flex-wrap">
         <div className="flex flex-col  my-6">
