476f14c87fee7ca02adb4f7378f06cbe81cba218
chai-nadig <chaitanya.nadig@gmail.com>
2021-11-29 17:29:09 -0800

    smaller traits

--- pages/index.js
+++ pages/index.js
@@ -101,30 +101,30 @@ return (
     {showTraits ? 
     <div id="traits" className="py-10">
       <h2 className="text-crypto-red text-5xl text-center">TRAITS</h2>
-      <div className="flex flex-wrap px-4 py-10">
-        <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5  grid justify-items-center">
+      <div className="flex flex-wrap px-6 py-10">
+        <div className="w-full md:w-1/3 lg:w-1/3 py-3 grid justify-items-center">
           <span class="text-white text-lg">{traits[0][0]}</span>
-          <img src={traits[0][1]} className="w-11/12 rounded-lg"/>
+          <img src={traits[0][1]} className="w-4/5 rounded-lg"/>
         </div>
-        <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5 grid justify-items-center">
+        <div className="w-full md:w-1/3 lg:w-1/3 py-3 grid justify-items-center">
           <span class="text-white text-lg">{traits[1][0]}</span>
-          <img src={traits[1][1]} className="w-11/12 rounded-lg"/>
+          <img src={traits[1][1]} className="w-4/5 rounded-lg"/>
         </div>
-        <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5  grid justify-items-center">
+        <div className="w-full md:w-1/3 lg:w-1/3 py-3 grid justify-items-center">
           <span class="text-white text-lg">{traits[2][0]}</span>
-          <img src={traits[2][1]} className="w-11/12 rounded-lg"/>
+          <img src={traits[2][1]} className="w-4/5 rounded-lg"/>
         </div>
-        <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5  grid justify-items-center">
+        <div className="w-full md:w-1/3 lg:w-1/3 py-3 grid justify-items-center">
           <span class="text-white text-lg">{traits[3][0]}</span>
-          <img src={traits[3][1]} className="w-11/12 rounded-lg"/>
+          <img src={traits[3][1]} className="w-4/5 rounded-lg"/>
         </div>
-        <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5  grid justify-items-center">
+        <div className="w-full md:w-1/3 lg:w-1/3 py-3 grid justify-items-center">
           <span class="text-white text-lg">{traits[4][0]}</span>
-          <img src={traits[4][1]} className="w-11/12 rounded-lg"/>
+          <img src={traits[4][1]} className="w-4/5 rounded-lg"/>
         </div>
-        <div className="w-full md:w-1/3 lg:w-1/3 py-3 px-5  grid justify-items-center">
+        <div className="w-full md:w-1/3 lg:w-1/3 py-3 grid justify-items-center">
           <span class="text-white text-lg">{traits[5][0]}</span>
-          <img src={traits[5][1]} className="w-11/12 rounded-lg"/>
+          <img src={traits[5][1]} className="w-4/5 rounded-lg"/>
         </div>
       </div>
     </div>
