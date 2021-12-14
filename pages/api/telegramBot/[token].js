f135a67e46cadd3672f2ee4ebe89934d88ea0ea8
Chai Nadig <chaitanya.nadig@gmail.com>
2021-12-13 19:43:35 -0800

    telegram bot

---
+++ pages/api/telegramBot/[token].js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { stringify } from "postcss";
+
+const Twitter = require('twitter-v2');
+
+const telegramBot = async (req, res) => {
+    if (req.query.token != process.env.TELEGRAM_WEBHOOK_TOKEN) {
+        res.statuscode = 401
+        res.json({ 'error': 'unathorized', 'message': 'invalid token' });
+        return
+    }
+
+    const message = req.body.message.text;
+
+    if (message === "/followers" || message === "/f") {
+        const client = new Twitter({
+            bearer_token: process.env.TWITTER_BEARER_TOKEN
+        });
+
+        const user = await client.get('users/1457120903695724545',{'user.fields': ['public_metrics']});
+        
+        const telegramToken = process.env.TELEGRAM_TOKEN
+        const telegramParams = {
+            'parse_mode': 'HTML',
+            'chat_id': parseInt(process.env.TELEGRAM_CHAT_ID),
+            'text': 'Followers: ' + JSON.stringify(user.data.public_metrics.followers_count)
+        }
+        
+        const res = await axios.get(`https://api.telegram.org/bot${telegramToken}/sendMessage`, { params: telegramParams })
+
+        if (res.status != 200) {
+            console.log(res);
+        }
+    }
+
+    res.json({ "message":"done" })
+}
+
+export default telegramBot    
